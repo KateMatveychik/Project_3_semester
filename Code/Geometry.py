1c5a3a1bd2dc66280c94011bcae613149e518e38
KateMatveychik <e-matveychik@yandex.ru>
2024-12-30 03:15:06 +0300

    grad fir cube 14x14x14

--- Code/Geometry.py
+++ Code/Geometry.py
@@ -103,7 +103,7 @@ def Rectangle_packing(Params:dict, Fill:bool = False) -> Dict[str, List[Ring]]:
                              j * delta_y + Shift_y  + r0['ny'],
                              k * delta_z + Shift_z  + r0['nz'],
                             orientation,
-                            r, w, L, C-3.0874506032437155*i*10**-10, R)
+                            r, w, L, C-5.292772462703512*i*10**-10, R)
                     )
         Numbers[orientation] = len(rings)
         Rings[orientation] = rings
